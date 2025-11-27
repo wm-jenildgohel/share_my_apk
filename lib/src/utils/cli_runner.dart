7b98b5325d17c8c9d63543a19ef2d7f49e7623e5
Jenil D Gohel <jenilg@webmobtech.com>
2025-11-27 15:06:00 +0530

    chore: suppress avoid_print lints for CLI interaction files

--- lib/src/utils/cli_runner.dart
+++ lib/src/utils/cli_runner.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: avoid_print
 import 'dart:io';
 
 import 'package:share_my_apk/share_my_apk.dart';
--- lib/src/utils/prompt_util.dart
+++ lib/src/utils/prompt_util.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: avoid_print
 import 'dart:io';
 
 /// Utility for interactive CLI prompts
